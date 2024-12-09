8792c4fff5cf66ef440df0390fc3ae962b053121
mastomas1202 <tomassasra02@gmail.com>
2024-12-09 12:06:37 +0700

    update commit

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -17,7 +17,7 @@ export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -26,7 +26,6 @@ export default async function CardWrapper() {
         value={numberOfCustomers}
         type="customers"
       />
-      </div>
     </>
   );
 }
